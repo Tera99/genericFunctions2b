45d846497a5b02a1bf116e586fba6f88c206cbf0
Tera Schaller <tschaller722@gmail.com>
2018-07-04 14:09:34 -0600

    Using generic programming with multiple data types

--- genericFunctions2/main.cpp
+++ genericFunctions2/main.cpp
@@ -5,11 +5,15 @@
 //  Created by Terezie Schaller on 7/4/18.
 //  Copyright © 2018 Terezie Schaller. All rights reserved.
 //
+//  Udemy.com, Advanced C++
+//  Class Templates with multiple data types
+//  Section 3, Lecture 21
 
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    
+    
     return 0;
 }
