d9482672bd21dd66cc613abf01a5baba01b0c75d
Tera Schaller <tschaller722@gmail.com>
2018-07-04 14:06:54 -0600

    Initial Commit

---
+++ genericFunctions2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  genericFunctions2
+//
+//  Created by Terezie Schaller on 7/4/18.
+//  Copyright © 2018 Terezie Schaller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
